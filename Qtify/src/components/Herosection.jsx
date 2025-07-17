29e4e8307b74835e88a0566d7b42cbcdd88b6d50
HemavathyLakisha <hemaluki@gmail.com>
2025-07-17 16:48:08 +0530

    Qtify hero section image and logo image path updated

--- Qtify/src/components/Herosection.jsx
+++ Qtify/src/components/Herosection.jsx
@@ -1,21 +1,41 @@
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import headphones from "../assets/hero_headphones.png"; // Correct path
 
 const Herosection = () => {
-    return (
-        <Stack spacing={2} direction="row" sx={{background:"#121212", height:"270px",display:"flex", alignItems:"center",  justifyContent:"center"}}>
-            <Box sx={{ width:"600px", marginLeft:"100px !important"}}>
-                <Typography variant="h4" gutterBottom sx={{color:"#ffffff", textAlign:"center",fontFamily: "Poppins"}}>
-                    100 thousand songs, ad-free <br />over thousands podcast episodes
-                </Typography>
-                
-            </Box>
-            <Box>
-                 <img alt="headphones" src="src\assets\hero_headphones.png" height="200" />  
-            </Box>
-        </Stack>
-    )
-}
+  return (
+    <Stack
+      spacing={2}
+      direction="row"
+      sx={{
+        background: "#121212",
+        height: "270px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        px: 2, // padding for small screens
+      }}
+    >
+      <Box sx={{ width: "600px", ml: "100px" }}>
+        <Typography
+          variant="h4"
+          gutterBottom
+          sx={{
+            color: "#ffffff",
+            textAlign: "center",
+            fontFamily: "Poppins",
+          }}
+        >
+          100 thousand songs, ad-free <br /> over thousands podcast episodes
+        </Typography>
+      </Box>
+
+      <Box>
+        <img alt="headphones" src={headphones} height="200" />
+      </Box>
+    </Stack>
+  );
+};
 
 export default Herosection;
--- Qtify/src/components/Logo.jsx
+++ Qtify/src/components/Logo.jsx
@@ -1,28 +1,41 @@
-
-import Avatar from '@mui/material/Avatar';
-import AvatarGroup from '@mui/material/AvatarGroup';
-import Stack from '@mui/material/Stack';
-
+import React from 'react';
+import { Avatar, AvatarGroup, Stack } from '@mui/material';
+import logo from '../assets/logo.png'; // Adjust path as needed
 
 const Logo = () => {
-    return ( 
-   
-            <Stack direction="row" spacing={2}>
-                <a href="#"><img alt="logo" src="src\assets\logo.png" height="40"/> </a>              
-            
-             <AvatarGroup total={3} sx={{ bgcolor:"#fefefe", borderRadius:"10px 10px 10px 0px", height:"38px", padding:"2px"}} spacing="small">
-                <Avatar sx={{ bgcolor: "#f011b8ff", width: 30, height: 30, marginTop : "3px" }} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                <Avatar sx={{ bgcolor: "#756c73ff", width: 30, height: 30, marginTop : "3px" }} alt="Travis Howard" src="/static/images/avatar/2.jpg" />               
-                <Avatar sx={{ bgcolor: "#dbd1d9ff", width: 30, height: 30, marginTop : "3px" }} alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-            </AvatarGroup> 
-            <AvatarGroup total={2} sx={{ bgcolor:"#fefefe", borderRadius:"10px 10px 10px 0px", height:"28px", padding:"2px"}} spacing="medium">
-                <Avatar sx={{ bgcolor: "#f011b8ff", width: 20, height: 20, marginTop : "3px" }} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-                <Avatar sx={{ bgcolor: "#756c73ff", width: 20, height: 20, marginTop : "3px" }} alt="Travis Howard" src="/static/images/avatar/2.jpg" />               
-            </AvatarGroup>
+  return (
+    <Stack direction="row" spacing={2} alignItems="center">
+      <a href="/">
+        <img alt="Qtify Logo" src={logo} height="40" />
+      </a>
 
-            </Stack>            
-                      
-    )
-}
+      <AvatarGroup
+        total={3}
+        sx={{
+          bgcolor: "#fefefe",
+          borderRadius: "10px 10px 10px 0px",
+          height: "38px",
+          padding: "2px",
+        }}
+      >
+        <Avatar
+          sx={{ bgcolor: "#f011b8ff", width: 30, height: 30 }}
+          alt="Remy Sharp"
+          src="/static/images/avatar/1.jpg"
+        />
+        <Avatar
+          sx={{ bgcolor: "#756c73ff", width: 30, height: 30 }}
+          alt="Travis Howard"
+          src="/static/images/avatar/2.jpg"
+        />
+        <Avatar
+          sx={{ bgcolor: "#dbd1d9ff", width: 30, height: 30 }}
+          alt="Trevor Henderson"
+          src="/static/images/avatar/5.jpg"
+        />
+      </AvatarGroup>
+    </Stack>
+  );
+};
 
 export default Logo;
